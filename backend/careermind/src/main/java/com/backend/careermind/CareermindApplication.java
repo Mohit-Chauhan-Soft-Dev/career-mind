515ccbc27eb1f47da05d57d2029a4829455766ee
Mohit Chauhan <mohit.chauhan.050607@gmail.com>
2025-07-06 15:54:04 +0530

    Changes

---
+++ backend/careermind/src/main/java/com/backend/careermind/CareermindApplication.java
@@ -0,0 +1,13 @@
+package com.backend.careermind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareermindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CareermindApplication.class, args);
+	}
+
+}
